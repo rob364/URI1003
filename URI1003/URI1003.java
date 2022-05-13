e624fd12e0a107f4b3b020e3d7d760dbc94c12ff
Roberta <vcindustria@gmail.com>
2022-05-13 15:45:33 -0300

    uri 1003

---
+++ URI1003/URI1003.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class URI1003 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		int A = input.nextInt();
+		int B = input.nextInt();
+		int SOMA = A + B;
+		
+		System.out.print("SOMA = " + SOMA);
+		
+		input.close();
+		
+	}
+
+}
